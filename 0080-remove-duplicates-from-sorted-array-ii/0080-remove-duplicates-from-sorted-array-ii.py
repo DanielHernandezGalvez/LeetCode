49435c7353dcacc533f52d2887371ae34144306c
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-10-24 13:15:41 -0600

    Time: 31 ms (74.42%), Space: 11.9 MB (15.31%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,19 +1,22 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-                # Edge case: if the length of nums is less than or equal to 2, return its length
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Edge case: if the array has fewer than 2 elements, return its length
         if len(nums) <= 2:
             return len(nums)
-        
-        # j is the index where the next allowed element should be placed
-        j = 2
-        
-        # Iterate over the array starting from the third element
+
+        # Initialize a pointer for the place to insert the next allowed duplicate
+        k = 2
+
+        # Start checking from the third element
         for i in range(2, len(nums)):
-            # If the current element nums[i] is different from nums[j-2]
-            # then it means nums[i] can be placed at index j
-            if nums[i] != nums[j - 2]:
-                nums[j] = nums[i]
-                j += 1
-        
-        # Return the length of the modified array
-        return j
+            # If the current element is not equal to the element at k-2, it's allowed to stay
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+
+        # Return the number of elements after removing duplicates (up to two occurrences per element)
+        return k
