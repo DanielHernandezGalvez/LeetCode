9869b2849971fa0f1c5e8152f32dca99d5639084
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-10-24 13:13:47 -0600

    Time: 0 ms (100.00%), Space: 13.1 MB (49.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,13 +1,22 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Edge case: if the array is empty, return 0
         if not nums:
             return 0
         
-        k = 1  
+        # Initialize a pointer to track the position of unique elements
+        k = 1
         
+        # Iterate over the array starting from the second element
         for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]: 
-                nums[k] = nums[i]  
+            # If the current element is different from the previous one, it's unique
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
                 k += 1
         
+        # Return the number of unique elements
         return k
